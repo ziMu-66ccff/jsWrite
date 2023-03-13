c60ce0d118fcdc7a0776c4040775d73b101ff49e
ziMu-66ccff <3553189793@qq.com>
2023-03-13 23:23:28 +0800

    补充

--- findNumberIn2DArray.js
+++ findNumberIn2DArray.js
@@ -1,3 +1,16 @@
 function findNumberIn2DArray(arr, target) {
-    if (arr === null || Array.isArray(arr))
+    if (arr === null || Array.isArray(arr)) return false;
+     
+    let row = 0;
+    let colum = arr[0].length - 1;
+
+    while (row < arr.length && colum >= 0) {
+        if (target === arr[row][colum]) return true;
+        if (target < arr[row][colum]) {
+            colum--;
+        } else {
+            row++;
+        }
+    }
+    return false;
 }
